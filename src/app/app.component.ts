fa8a6bdb9fefed4d104a20eb51352289e81dc5f1
Eikyuu <v.duguet.dev@gmail.com>
2020-02-02 17:36:12 +0100

    router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from '@angular/core';
-import { OnInit } from "@angular/core";
 
 import { Pokemon } from "./Pokemon";
 import { POKEMONS } from './mock-pokemons';
 
 @Component({
-  selector: 'app-root',
+  selector: 'pokemon-app',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
-  
-  private pokemons: Pokemon[];
-  private title: string = "Liste des pokemons";
-
-  ngOnInit() {
-
-    this.pokemons = POKEMONS;
-
-  }
-
-  selectPokemon(pokemon: Pokemon) {
-    alert("vous avez cliquer sur " + pokemon.name);
-  }
+export class AppComponent  {
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BorderCardDirective } from './border-card.directive';
 import { PokemonTypeColorPipe } from './pokemon-type-color.pipe';
+import { DetailPokemonComponent } from './detail-pokemon/detail-pokemon.component';
+import { ListPokemonComponent } from './list-pokemon/list-pokemon.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BorderCardDirective,
-    PokemonTypeColorPipe
+    PokemonTypeColorPipe,
+    DetailPokemonComponent,
+    ListPokemonComponent
   ],
   imports: [
     BrowserModule,
